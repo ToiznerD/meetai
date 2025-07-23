5304ae400b8d89671025d68580105176689f8915
Dor Toizner <dor@neurokaire.com>
2025-07-23 19:08:04 +0300

    add meetings form

--- src/modules/meetings/server/procedures.ts
+++ src/modules/meetings/server/procedures.ts
@@ -5,6 +5,7 @@ import z from "zod";
 import { and, count, desc, eq, getTableColumns, ilike } from "drizzle-orm";
 import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@/constants";
 import { TRPCError } from "@trpc/server";
+import { meetingsInsertSchema, meetingsUpdateSchema } from "../schemas";
 
 export const meetingsRouter = createTRPCRouter({
     // TODO: Change 'getMany' to use protectedProcedure
@@ -70,5 +71,40 @@ export const meetingsRouter = createTRPCRouter({
             throw new TRPCError({ code: "NOT_FOUND", message: "Agent not found" })
         }
         return existingMeeting;
+    }),
+    create: protectedProcedure
+        .input(meetingsInsertSchema)
+        .mutation(async ({ input, ctx }) => {
+            const [createdMeeting] = await db
+                .insert(meetings)
+                .values({
+                    ...input,
+                    userId: ctx.auth.user.id,
+                })
+                .returning();
+
+            // TODO: Create stream call, upsert stream users
+            
+            return createdMeeting;
+        }),
+    update: protectedProcedure
+        .input(meetingsUpdateSchema)
+        .mutation(async ({ ctx, input }) => {
+            const [updatedMeeting] = await db
+                .update(meetings)
+                .set(input)
+                .where(
+                    and(
+                        eq(meetings.id, input.id),
+                        eq(meetings.userId, ctx.auth.user.id)
+                    )
+                )
+                .returning();
+            
+            if (!updatedMeeting){
+                throw new TRPCError({ code: "NOT_FOUND", message: "Meeting not found"})
+            }
+
+            return updatedMeeting;
     }),
 })
