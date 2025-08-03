cdb8ce77016ce54cfcb311a4ad89ab31e4cbde88
Dor Toizner <dor@neurokaire.com>
2025-08-03 14:25:15 +0300

    add meeting id page

--- src/modules/meetings/server/procedures.ts
+++ src/modules/meetings/server/procedures.ts
@@ -77,8 +77,12 @@ export const meetingsRouter = createTRPCRouter({
         const [existingMeeting] = await db
             .select({
                 ...getTableColumns(meetings),
+                agent: agents,
+                duration: sql<number>`EXTRACT(EPOCH FROM (ended_at - started_at))`.as("duration"),
+
             })
             .from(meetings)
+            .innerJoin(agents, eq(meetings.agentId, agents.id))
             .where(and(
                 eq(meetings.id, input.id),
                 eq(meetings.userId, ctx.auth.user.id)
@@ -123,5 +127,24 @@ export const meetingsRouter = createTRPCRouter({
             }
 
             return updatedMeeting;
+    }),
+    remove: protectedProcedure
+        .input(z.object({ id: z.string() }))
+        .mutation(async ({ ctx, input }) => {
+            const [removedMeeting] = await db
+                .delete(meetings)
+                .where(
+                    and(
+                        eq(meetings.id, input.id),
+                        eq(meetings.userId, ctx.auth.user.id)
+                    )
+                )
+                .returning();
+            
+            if (!removedMeeting){
+                throw new TRPCError({ code: "NOT_FOUND", message: "Meeting not found"})
+            }
+
+            return removedMeeting;
     }),
 })
